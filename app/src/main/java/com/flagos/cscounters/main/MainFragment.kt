5f9eb0f2f3cc0682edb0f08b265076599dccab82
Alejandro Lagos <flagos.rd@gmail.com>
2021-04-02 18:21:31 -0600

    Added goToCreateItem action

--- app/src/main/java/com/flagos/cscounters/main/MainFragment.kt
+++ app/src/main/java/com/flagos/cscounters/main/MainFragment.kt
@@ -5,11 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.flagos.cscounters.R
+import androidx.navigation.fragment.findNavController
+import com.flagos.cscounters.databinding.FragmentMainBinding
+import com.flagos.cscounters.main.MainFragmentDirections.Companion.actionMainFragmentDestToCreateItemFragmentDest
 
 class MainFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.buttonMainAddCounter.setOnClickListener { goToCreateItemScreen() }
+    }
+
+    private fun goToCreateItemScreen() {
+        val action = actionMainFragmentDestToCreateItemFragmentDest()
+        findNavController().navigate(action)
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 }
